6789683a3a142f34cdb2fdc970d2c277a53e78cd
Xinnot <63211896+Xinnot@users.noreply.github.com>
2021-04-15 20:19:28 -0300

    Add files via upload

---
+++ Alura Academy/JavaScript/01 conversor-de-moedas/src/script.js
@@ -0,0 +1,21 @@
+var precoDolar = 5.50
+
+alert("Preço do Dolar Atual:" + precoDolar)
+
+var precoDolarPer = prompt("Qual o seu preço?")
+
+var dolarComoNumero = parseFloat(precoDolarPer)
+
+dolarComoNumero = dolarComoNumero * precoDolar
+
+alert("R$" + dolarComoNumero.toFixed(2))
+
+//Revisão
+//Vang
+//Revisão
+//Variáveis var int - float - string
+//alert - parseInt - parseFloat - prompt
+//Operações + soma * multiplicar
+//Comentário finalert - parseInt - parseFloat - prompt
+//Operações + soma * multiplicar
+//Comentário final
